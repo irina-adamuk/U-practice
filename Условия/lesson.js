765bac9a33b79c4f9b698cbcc47b980d88e770c0
Irina Adamuk <grinirina032@gmail.com>
2021-03-04 18:16:56 +0300

    Switch constraction

---
+++ Условия/lesson.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const num = 50;
+
+switch (num) {
+   case 49:
+      console.log('Wrong');
+      break;
+   case 100:
+      console.log('Wrong');
+      break;
+   case 50:
+      console.log('You are wright');
+   default:
+      console.log('Not this time!');
+      break;
+}
