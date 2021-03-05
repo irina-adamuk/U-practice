68c5a27a8cd50968b8ce025869ba61d57cc47928
Irina Adamuk <grinirina032@gmail.com>
2021-03-05 17:55:04 +0300

    Script

---
+++ Objects/object_oriented_programming.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let str = 'some';
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+   health: 400,
+   armor: 100,
+};
+const john = Object.create(soldier);
+
+// const john = {
+//    health: 100
+// };
+
+// Object.setPrototypeOf(john, soldier);
+// john.__proto__ = soldier; старый формат
+
+
+
