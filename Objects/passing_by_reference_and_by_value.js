1b7ac5d44a7f72d3df31e47730e0397195f48646
Irina Adamuk <grinirina032@gmail.com>
2021-03-05 17:26:25 +0300

    Script of arr and obj passing

---
+++ Objects/passing_by_reference_and_by_value.js
@@ -0,0 +1,91 @@
+'use strict';
+
+let a = 5,
+   b = a;
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+   a: 5,
+   b: 1,
+};
+
+// const copy = obj; // ссылка
+
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+   let objCopy = {};
+   let key;
+   for(key in mainObj) {
+      objCopy[key] = mainObj[key];
+   }
+   return objCopy;
+}
+
+const numbers = {
+   a: 2,
+   b: 5,
+   c: {
+      x: 7,
+      y: 4,
+   },
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.b = 77;
+console.log(newNumbers);
+
+
+const add = {
+   d: 17,
+   e: 25,
+};
+console.log(Object.assign(numbers, add));
+const clone = Object.assign({}, add);
+
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'krjdhgjdfhgjkfdhjg fhvjufhjbfghb';
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+   console.log(a);
+   console.log(b);
+   console.log(c);
+}
+
+const num = [2, 5, 7];
+log(...num);
+
+
+const array = ['a', 'b'];
+
+const newARRRR = [...array];
+
+const g = {
+   one: 1,
+   two: 2,
+};
+
+const newObj = {...q};
+
+
+
+
+
