2b5fc16eabc15057319f50575cc1a58b78a00ac1
Irina Adamuk <grinirina032@gmail.com>
2021-03-05 12:37:53 +0300

    Sctipt about nmbers and string

---
+++ Numbers_and_strings/Methods_and_prop_of_numb_and_string.js
@@ -0,0 +1,33 @@
+'use strict';
+// ? string
+const str = 'test';
+const arr = [1, 2, 3]
+// свойство строки
+console.log(str.length);
+// console.log(arr.length);
+console.log(str[2]);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+let fruit = 'Some fruit';
+console.log(fruit.indexOf('fruit'));
+
+const logg = "Hello world, I love you very much";
+console.log(logg.slice(6,11));
+console.log(logg.slice(0,6));
+console.log(logg.slice(15,19)); //love
+console.log(logg.slice(24,28)); // very
+console.log(logg.slice(13));
+
+console.log(logg.substring(24, 28));
+console.log(logg.substr(6, 5)); // с какого символа и сколько символов нужно вырезать.
+
+// ? Number
+
+console.log();
+
+
+
+
