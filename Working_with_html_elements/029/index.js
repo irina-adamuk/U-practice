7bd8d6dc4d57b1feb3674f91c1c9aa0a81acf92a
Irina Adamuk <grinirina032@gmail.com>
2021-03-09 16:09:15 +0300

    relocation of elemrnts

---
+++ Working_with_html_elements/029/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const box = document.getElementById('Box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
+
+console.dir(box);
