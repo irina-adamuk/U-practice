89e6773cac0b643367b34d9a19854a990f3b1b74
Irina Adamuk <grinirina032@gmail.com>
2021-03-10 10:29:09 +0300

    commit

--- Working_with_html_elements/029/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const box = document.getElementById('Box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart');
-
-console.dir(box);
