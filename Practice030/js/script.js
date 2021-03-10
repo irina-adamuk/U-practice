2fad5558ad9ce098304e016f0814036a7704bccd
Irina Adamuk <grinirina032@gmail.com>
2021-03-10 11:30:35 +0300

    Script of practice

--- Practice030/js/script.js
+++ Practice030/js/script.js
@@ -20,7 +20,51 @@ const movieDB = {
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
+        "Призрак",
+        "Титаник",
+        "Русалочка"
     ]
 };
 
+// ? 1)
+const   adv = document.querySelectorAll('.promo__adv img'),
+// это нам понадобится для задания 3
+        poster = document.querySelector('.promo__bg'),
+// для задания 2
+        genre = poster.querySelector('.promo__genre'),
+// для задания 4
+        movieList = document.querySelector('.promo__interactive-list');
+
+
+adv.forEach(item => {
+    item.remove();
+});
+
+// или 
+// adv.forEach(function(item) {
+//     item.remove();
+// });
+
+// ? 2)
+
+genre.textContent = 'драма';
+
+// ? 3)
+// мы поменяли путь к картинки
+poster.style.backgroundImage = "url('img/bg.jpg')"; 
+
+// ? 4)
+
+movieList.innerHTML = '';
+// сортируем фильмы в массиве с помощью метода sort()
+movieDB.movies.sort();
+
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
